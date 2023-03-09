0b377faf02a2c3daf9454183846dda3b936d774e
Max <maxjordan@hotmail.se>
2023-03-09 10:29:15 +0100

    Skapat JSON Object

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
-
+import org.json.simple.JSONObject;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is an example");
-        System.out.println("Hej");
+
+        //Skapa ett JSON objekt
+        JSONObject jsonOb = new JSONObject();
+
+        //Spara värden i JSON Object
+        jsonOb.put("namn", "Sebbe");
+        jsonOb.put("age", 22);
+
+        //Skriva ut värden
+        System.out.println("Mitt namn är: " + jsonOb.get("namn"));
+        System.out.printf("Jag är " + jsonOb.get("age") + " år gammal");
+
+
     }
 }
